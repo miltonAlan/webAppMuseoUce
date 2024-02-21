8437f4ebcd77d0876bccf3286d53575629fba425
mpaucar <tu@email.com>
2024-02-20 19:17:21 -0500

    commit funcional

--- app.js
+++ app.js
@@ -9,7 +9,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// Configurar la carpeta estática para servir archivos estáticos como JS, imágenes, etc.
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Configurar la carpeta estática del build para servir archivos generados por React (ej. JS, imágenes, CSS)
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Configurar el motor de vistas y las rutas
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -17,25 +23,30 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+// Configurar las rutas de tu aplicación
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 y pasar al manejador de errores
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Manejador de errores
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // set locals, solo proporcionar error en desarrollo
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // renderizar la página de error
   res.status(err.status || 500);
   res.render('error');
+});
+
+// Configurar la regla para servir el index.html para todas las demás rutas
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 module.exports = app;
